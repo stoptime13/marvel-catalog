ba4455b4b581dbd5e4915a684afd9cf6604367c4
Anastasia <stoptime13@list.ru>
2022-08-14 17:48:49 +0300

    added error handling and spinner

---
+++ src/components/errorMessage/errorMessage.js
@@ -0,0 +1,21 @@
+import img from './error.gif'
+
+const ErrorMessage = () => {
+    return(
+        <h3 style={{
+                display: 'block',
+                width: '90px',
+                height: '40px',
+                objectFit: 'contain',
+                margin: '0 auto',
+                fontFamily: 'Marvel regular',
+                fontSize: '40px',
+                fontWeight: '400'
+            }
+        }>
+            ERROR
+        </h3>
+    )
+}
+
+export default ErrorMessage;
