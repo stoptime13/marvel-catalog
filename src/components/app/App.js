356ddb94a61ef782cf969b2b52c41df8ce1c2ac5
Anastasia <stoptime13@list.ru>
2022-08-22 14:26:42 +0300

    add comicsList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,9 @@
 import {useState} from "react";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
-import decoration from '../../resources/img/vision.png';
+import ComicsList from "../comicsList/ComicsList";
 
 const App = () => {
     const [selectedChar, setChar] = useState(null);
@@ -19,18 +16,21 @@ const App = () => {
         <div className="app">
             <AppHeader/>
             <main>
+                {/*<ErrorBoundary>*/}
+                {/*    <RandomChar/>*/}
+                {/*</ErrorBoundary>*/}
+                {/*<div className="char__content">*/}
+                {/*    <ErrorBoundary>*/}
+                {/*        <CharList onCharSelected={onCharSelected}/>*/}
+                {/*    </ErrorBoundary>*/}
+                {/*    <ErrorBoundary>*/}
+                {/*        <CharInfo selectChar={selectedChar}/>*/}
+                {/*    </ErrorBoundary>*/}
+                {/*</div>*/}
+                {/*<img className="bg-decoration" src={decoration} alt="vision"/>*/}
                 <ErrorBoundary>
-                    <RandomChar/>
+                    <ComicsList/>
                 </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo selectChar={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
         </div>
     )
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -7,15 +7,24 @@ const useMarvelService = () => {
     const _apiKey = 'apikey=97c93012fc9de659a16cfdddeebf2ca3';
     const _baseOffset = 210;
 
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
+    }
 
     const getAllCharacters = async (offset = _baseOffset) => {
         const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformCharacter);
     }
 
-    const getCharacter = async (id) => {
-        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
-        return _transformCharacter(res.data.results[0]);
+    const getComics = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0])
+    }
+
+    const getAllComics = async (offset = 0) => {
+        const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
     }
 
     const _transformCharacter = (char) => {
@@ -31,7 +40,19 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError};
+    const _transformComics = (comicsItem) => {
+        return{
+            id: comicsItem.id,
+            title: comicsItem.title,
+            description: comicsItem.description || 'There is no description',
+            pageCount: comicsItem.pageCount ? `${comicsItem.pageCount} p.` : 'No information about the number of pages',
+            thumbnail: comicsItem.thumbnail.path + '.' + comicsItem.thumbnail.extension,
+            language: comicsItem.textObjects.language || 'en-us',
+            price: comicsItem.prices.price ? `${comicsItem.prices.price}$` : 'not available'
+        }
+    }
+
+    return {loading, error, getAllCharacters, getCharacter, clearError, getComics, getAllComics};
 }
 
 export default useMarvelService;
